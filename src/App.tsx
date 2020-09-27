c516206feb5e029611ee3e93863347430cdea7b3
filip4428 <filip.strelec@speck.agency>
2020-09-27 22:58:23 +0200

    Initial setup (BrowserRouter, styled comp...)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { GlobalStyle } from "./lib/style/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <GlobalStyle />
+<Route path ="/" exact component={()=>{return(<div>Landing</div>) }}/>
+<Route path ="/pool" exact component={()=>{return(<div>Pool</div>) }}/>
+
+      <Switch></Switch>
+    </BrowserRouter>
   );
 }
 
