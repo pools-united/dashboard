e994e196762bc6f1dbc948e8ea8da0559f2fbe80
Dario Uršulin <dario.ursulin@gmail.com>
2020-10-14 19:51:41 +0200

    feat: improve cards

--- src/assets/jss/material-kit-react/components/cardPoolStyle.js
+++ src/assets/jss/material-kit-react/components/cardPoolStyle.js
@@ -54,16 +54,12 @@ const cardStyle = {
     borderRadius: "6px",
     color: "rgba(0, 0, 0, 0.87)",
     background: "#fff",
-    boxShadow:
-      "0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 1px 5px 0 rgba(0, 0, 0, 0.12)",
-    position: "relative",
     display: "flex",
     flex: "1 1 auto",
     flexDirection: "column",
     // minWidth: "380px",
     wordWrap: "break-word",
     fontSize: ".875rem",
-    transition: "all 300ms linear",
     padding: "0px 24px 24px 24px",
   },
   cardPlain: {
