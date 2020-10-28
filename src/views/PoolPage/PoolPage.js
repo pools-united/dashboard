8c7a6a1499fcc78479b9522e13ffb42ca596dc28
filip4428 <filip.strelec@speck.agency>
2020-10-28 21:51:03 +0100

    Implement era pool and small fixes

--- src/views/PoolPage/PoolPage.js
+++ src/views/PoolPage/PoolPage.js
@@ -25,7 +25,10 @@ import Chart from "react-apexcharts";
 
 //images
 import VenusBanner from "assets/poolAssets/venus/FrescoBanner.png";
+import EraBanner from "assets/poolAssets/era/EraBanner.png";
 import VenusLogo from "assets/poolAssets/venus/fresco_logo.png";
+import EraLogo from "assets/poolAssets/era/era_logo.png";
+
 import FileCopyIcon from "@material-ui/icons/FileCopy";
 
 //components
@@ -122,6 +125,7 @@ const AbsoluteLogo = styled.img`
   @media (max-width: 960px) {
     display: unset;
     position: absolute;
+    padding: 68px;
     width: 100%;
     z-index: -1;
     object-fit: contain;
@@ -205,12 +209,17 @@ const RewardsInputComponent = styled.input`
   color: ${(props) => props.color};
   width: 30%;
   font-size: 18px;
+  border: 0;
 `;
 
 const ListItemStyled = styled(ListItem)`
   padding-left: 5px;
   padding-right: 5px;
   width: max-content;
+`;
+
+const DescriptionText = styled.h4`
+  font-weight: 400;
 `;
 
 const TooltipStyled = styled(Tooltip)`
@@ -245,6 +254,8 @@ const PoolPage = (props) => {
       name: "Fresco Pool |VENUS|",
       poolColor: "black",
       secondaryColor: "#1F2833",
+      bodyBackgroundColor: "#45A29E",
+
       logoColor: "#45A29E",
       description: (
         <>
@@ -276,6 +287,33 @@ const PoolPage = (props) => {
     CPU: {
       name: "Cardano Pools United |CPU|",
       description: "insert desc here",
+    },
+    ERA: {
+      name: "Nova Pool |ERA|",
+      poolColor: "black",
+      secondaryColor: "#000",
+      logoColor: "#0D4022",
+      bodyBackgroundColor: "#0D4022",
+      description: (
+        <>
+          We're two sisters handling one Nova |ERA| pool. We have been ADA
+          holders since 2017, now we decieded to collaborate with CPU team and
+          try our best to contribute to Cardano family.
+        </>
+      ),
+      descriptionMobile: (
+        <>
+          We're two sisters handling one Nova |ERA| pool. We have been ADA
+          holders since 2017, now we decieded to collaborate with CPU team and
+          try our best to contribute to Cardano family.
+        </>
+      ),
+
+      banner: EraBanner,
+      logo: EraLogo,
+      id: "19cb138eab81d3559e70094df2b6cb1742bf275e920300d5c3972253",
+      twitter: "https://twitter.com/ERA_NovaPool",
+      telegram: "https://t.me/CPUPools",
     },
   };
 
@@ -429,11 +467,15 @@ const PoolPage = (props) => {
   return (
     <AppContext.Consumer>
       {(context) => {
-        // context.poolStats[urlParams.id] &&
-        //   // console.log(context.poolStats[urlParams.id].data);
-        //   console.log(
+        console.log(context.poolStats);
+        context.poolStats[urlParams.id] &&
+          console.log(context.poolStats[urlParams.id]);
+        // console.log(
         //     JSON.parse(context.poolStats[urlParams.id].data.hist_bpe)
         //   );
+
+        //TODO: ovo mozda nebi trebalo tu biti, usporava render brijem!!!
+        //TODO: !!!
 
         if (context.globalStats.epoch_last && definedRender < 1) {
           setDefinedRender(definedRender + 1);
@@ -448,8 +490,6 @@ const PoolPage = (props) => {
           // console.log(epochsGraph);
         }
 
-        //TODO: ovo mozda nebi trebalo tu biti, usporava render brijem!!!
-        //TODO: !!!
         if (!numberOfBlocks && context.poolStats[urlParams.id]) {
           setNumberOfBlocks([]);
           setRoaStats([]);
@@ -509,7 +549,7 @@ const PoolPage = (props) => {
         return (
           <div>
             <PoolPageStyle
-              backgroundColor={poolsDetails[urlParams.id].logoColor}
+              backgroundColor={poolsDetails[urlParams.id].bodyBackgroundColor}
               backgroundColorHeader="black"
             />
             <Header
@@ -540,12 +580,12 @@ const PoolPage = (props) => {
                       {" "}
                       {urlParams && poolsDetails[urlParams.id].name}
                     </h1>
-                    <h4>
+                    <DescriptionText>
                       {!mobileState
                         ? urlParams && poolsDetails[urlParams.id].description
                         : urlParams &&
                           poolsDetails[urlParams.id].descriptionMobile}
-                    </h4>
+                    </DescriptionText>
                     <br />
                     <AbsoluteLogo src={poolsDetails[urlParams.id].logo} />
                     <IdWrapper
@@ -930,7 +970,9 @@ const PoolPage = (props) => {
                     },
                     colors: [
                       poolsDetails[urlParams.id].secondaryColor
-                        ? poolsDetails[urlParams.id].secondaryColor
+                        ? poolsDetails[urlParams.id].secondaryColor == "#000"
+                          ? "#808281"
+                          : poolsDetails[urlParams.id].secondaryColor
                         : "nikaj onda",
                       poolsDetails[urlParams.id].logoColor
                         ? poolsDetails[urlParams.id].logoColor
@@ -939,7 +981,9 @@ const PoolPage = (props) => {
                     fill: {
                       colors: [
                         poolsDetails[urlParams.id].secondaryColor
-                          ? poolsDetails[urlParams.id].secondaryColor
+                          ? poolsDetails[urlParams.id].secondaryColor == "#000"
+                            ? "#808281"
+                            : poolsDetails[urlParams.id].secondaryColor
                           : "nikaj onda",
                         poolsDetails[urlParams.id].logoColor
                           ? poolsDetails[urlParams.id].logoColor
