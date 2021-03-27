bb87b1ff0c9c2c76a69db124c67b52a230498784
filip4428 <filip.strelec@speck.agency>
2021-03-27 23:42:52 +0100

    Add Zvone

--- src/views/LandingPage/Sections/TeamSection.js
+++ src/views/LandingPage/Sections/TeamSection.js
@@ -21,6 +21,7 @@ import team2 from "assets/img/faces/Filip.jpg";
 import team3 from "assets/img/faces/Nora.jpg";
 import emil from "assets/img/faces/Emil.jpg";
 import team4 from "assets/img/faces/Miha.jpg";
+import zvonimir from "assets/img/faces/Zvonimir.jpg"
 
 const useStyles = makeStyles(styles);
 
@@ -131,9 +132,56 @@ export default function TeamSection() {
               </h4>
               <CardBody>
                 <p className={classes.description}>
-                  Mihovil is a React developer from Croatia, entered in the crypto sphere in 2017 as a ICO investor. 
-                  Fascinated by the <a href="https://cardano.org/">Cardano</a> blockchain, now playing an active role 
-                  in Cardano community as a owner of <a href="/pool?id=PROTO">Proto Pool.</a>
+                  Mihovil is a React developer from Croatia, entered in the
+                  crypto sphere in 2017 as a ICO investor. Fascinated by the{" "}
+                  <a href="https://cardano.org/">Cardano</a> blockchain, now
+                  playing an active role in Cardano community as a owner of{" "}
+                  <a href="/pool?id=PROTO">Proto Pool.</a>
+                </p>
+              </CardBody>
+              <CardFooter className={classes.justifyCenter}>
+                {/* <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-twitter"} />
+                </Button>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-instagram"} />
+                </Button>
+                <Button
+                  justIcon
+                  color="transparent"
+                  className={classes.margin5}
+                >
+                  <i className={classes.socials + " fab fa-facebook"} />
+                </Button> */}
+              </CardFooter>
+            </Card>
+          </GridItem>{" "}
+          <GridItem xs={12} sm={12} md={4}>
+            <Card plain>
+              <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
+                <img src={emil} alt="..." className={imageClasses} />
+              </GridItem>
+              <h4 className={classes.cardTitle}>
+                Emil Balint
+                <br />
+              </h4>
+              <CardBody>
+                <p className={classes.description}>
+                  Software engineer from Croatia with massive respect for
+                  Cardano community and its projects. Also a proud Cardano pool
+                  owner of &nbsp;
+                  <a href="/pool?id=MINES" target="_blank">
+                    Mines Pool
+                  </a>
+                  . He is also hell of a wake boarder and tennis player.
                 </p>
               </CardBody>
               <CardFooter className={classes.justifyCenter}>
@@ -161,31 +209,24 @@ export default function TeamSection() {
               </CardFooter>
             </Card>
           </GridItem>
-
-          <TwoCards>
-            {" "}
-            <GridItem xs={12} sm={12} md={4}>
-              <Card plain>
-                <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
-                  <img src={emil} alt="..." className={imageClasses} />
-                </GridItem>
-                <h4 className={classes.cardTitle}>
-                  Emil Balint
-                  <br />
-                </h4>
-                <CardBody>
-                  <p className={classes.description}>
-                    Software engineer from Croatia with massive respect for
-                    Cardano community and its projects. Also a proud Cardano
-                    pool owner of &nbsp;
-                    <a href="/pool?id=MINES" target="_blank">
-                      Mines Pool
-                    </a>
-                    . He is also hell of a wake boarder and tennis player.
-                  </p>
-                </CardBody>
-                <CardFooter className={classes.justifyCenter}>
-                  {/* <Button
+          <GridItem xs={12} sm={12} md={4}>
+            <Card plain>
+              <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
+                <img src={team3} alt="..." className={imageClasses} />
+              </GridItem>
+              <h4 className={classes.cardTitle}>
+                Nora Mlinarić
+                <br />
+              </h4>
+              <CardBody>
+                <p className={classes.description}>
+                  After earning her Bachelor's degree in Journalism, Nora went
+                  for Master's in PR. She entered the Crypto world to explore
+                  her passion for technology, digital marketing and writing.{" "}
+                </p>
+              </CardBody>
+              <CardFooter className={classes.justifyCenter}>
+                {/* <Button
                   justIcon
                   color="transparent"
                   className={classes.margin5}
@@ -206,27 +247,29 @@ export default function TeamSection() {
                 >
                   <i className={classes.socials + " fab fa-facebook"} />
                 </Button> */}
-                </CardFooter>
-              </Card>
-            </GridItem>
-            <GridItem xs={12} sm={12} md={4}>
-              <Card plain>
-                <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
-                  <img src={team3} alt="..." className={imageClasses} />
-                </GridItem>
-                <h4 className={classes.cardTitle}>
-                  Nora Mlinarić
-                  <br />
-                </h4>
-                <CardBody>
-                  <p className={classes.description}>
-                    After earning her Bachelor's degree in Journalism, Nora went
-                    for Master's in PR. She entered the Crypto world to explore
-                    her passion for technology, digital marketing and writing.{" "}
-                  </p>
-                </CardBody>
-                <CardFooter className={classes.justifyCenter}>
-                  {/* <Button
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <Card plain>
+              <GridItem xs={12} sm={12} md={6} className={classes.itemGrid}>
+                <img src={zvonimir} alt="..." className={imageClasses} />
+              </GridItem>
+              <h4 className={classes.cardTitle}>
+                Zvonimir Mlinaric
+                <br />
+              </h4>
+              <CardBody>
+                <p className={classes.description}>
+                  Zvonimir is a young scientist and soon to be Master of
+                  pharmacy who realized the potential of new technologies like
+                  blockchain in science and biomedicine and therefore founded
+                  his Marie Curie Pool. In his free time, he likes to watch a
+                  good movie and drink a good beer in a good company.
+                </p>
+              </CardBody>
+              <CardFooter className={classes.justifyCenter}>
+                {/* <Button
                   justIcon
                   color="transparent"
                   className={classes.margin5}
@@ -247,10 +290,9 @@ export default function TeamSection() {
                 >
                   <i className={classes.socials + " fab fa-facebook"} />
                 </Button> */}
-                </CardFooter>
-              </Card>
-            </GridItem>
-          </TwoCards>
+              </CardFooter>
+            </Card>
+          </GridItem>
         </GridContainer>
       </div>
     </div>
