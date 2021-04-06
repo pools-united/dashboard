e5e445769d72cd82d647e03f5a64b9273ed27c9b
Mihovil Santek <mihovilsantek@Mihovils-MacBook-Pro.local>
2021-04-06 10:21:04 +0200

    add form validation

--- src/views/ContactUs/EmailJSForm.js
+++ src/views/ContactUs/EmailJSForm.js
@@ -33,7 +33,7 @@ export default function ContactUs() {
                             <input type="text" className="form-control" placeholder="Subject" name="subject" required/>
                         </div>
                         <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message" required></textarea>
                         </div>
                         <div className="col-8 pt-3 mx-auto">
                             <input type="submit" className="btn btn-info" value="Send Message"></input>
