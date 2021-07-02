40137167b482204583f248d6f505f27b141ec690
Filip Strelec <filip.strelec@gmail.com>
2021-07-02 20:20:19 +0200

    Minor deploy fix

--- src/views/Cpu_donations/CpuDonationsPage.js
+++ src/views/Cpu_donations/CpuDonationsPage.js
@@ -18,7 +18,6 @@ import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import YAML from "yamljs"
 import styled from "styled-components";
 import styles from "assets/jss/material-kit-react/views/faqPage.js";
 
