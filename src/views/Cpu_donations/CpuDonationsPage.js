52e488edc682098f703cde784c542266eab9c16c
Filip Strelec <filip.strelec@gmail.com>
2021-07-09 19:15:14 +0200

    Code cleanup

--- src/views/Cpu_donations/CpuDonationsPage.js
+++ src/views/Cpu_donations/CpuDonationsPage.js
@@ -17,77 +17,9 @@ import styles from "assets/jss/material-kit-react/views/faqPage.js";
 
 //ZA FILIPA REMINDER: installiral si si yaml2json cli i s komandom yaml2json cpu.yaml > cpu.json updateas json file! 
 // import cpuDonationsJson from './cpu.json';
-import cpuExampleJson from './cpu.json';
-
-import ReactPlayer from "react-player";
+import cpuDonationsJson from './cpu.json';
 
 
-
-
-const timelineData = [
-  {
-    text: 'Started working on the app-ideas repository',
-    date: 'February 25 2019',
-    category: {
-      tag: 'app-ideas',
-      color: '#FFDB14'
-    },
-    link: {
-      url: 'https://github.com/florinpop17/app-ideas',
-      text: 'Check it out on GitHub'
-    }
-  },
-  {
-    text: 'Started the Weekly Coding Challenge program',
-    date: 'March 04 2019',
-    category: {
-      tag: 'blog',
-      color: '#e17b77'
-    },
-    link: {
-      url: 'https://florin-pop/blog/2019/03/weekly-coding-challenge/',
-      text: 'Check it out here'
-    }
-  },
-  {
-    text: 'Got 1.000 followers on Twitter',
-    date: 'March 07 2019',
-    category: {
-      tag: 'twitter',
-      color: '#1DA1F2'
-    },
-    link: {
-      url: 'https://twitter.com/florinpop1705',
-      text: 'See profile'
-    }
-  },
-  {
-    text:
-      'I published my first article in the FreeCodeCamp Medium Publication',
-    date: 'March 18 2019',
-    category: {
-      tag: 'medium',
-      color: '#018f69'
-    },
-    link: {
-      url:
-        'https://medium.freecodecamp.org/how-to-build-a-double-slider-sign-in-and-sign-up-form-6a5d03612a34',
-      text: 'Check it out here'
-    }
-  },
-  {
-    text: 'Over 12.000 views in a single day on my Medium posts',
-    date: 'April 05 2019',
-    category: {
-      tag: 'medium',
-      color: '#018f69'
-    },
-    link: {
-      url: 'https://medium.com/@popflorin1705',
-      text: 'See profile'
-    }
-  }
-]
 
 
 const TimelineItem = ({ data, dateTest }) => (
@@ -116,9 +48,9 @@ const TimelineItem = ({ data, dateTest }) => (
 
 const Timeline = () =>
 
-  cpuExampleJson.length > 0 && (
+  cpuDonationsJson.length > 0 && (
     <div className="timeline-container">
-      {cpuExampleJson.map((data, idx) => {
+      {cpuDonationsJson.map((data, idx) => {
         // let date = new Date( data.timestamp * 1000);
         // var hours = date.getHours();
         // // Minutes part from the timestamp
@@ -173,16 +105,6 @@ const FaqContainer = styled.div`
 export default function ProfilePage(props) {
   const classes = useStyles();
   const { ...rest } = props;
-
-  const [donationData, setDonationData] = useState();
-  useEffect(() => {
-    setDonationData(cpuExampleJson);
-    //MIHA LOG ZA TEBE, cpuExampleJson koristi za developanje a cpuDonationsJson ssu bas nase donacije
-    console.log(donationData);
-  }, [donationData]);
-
-
-
 
 
   return (
